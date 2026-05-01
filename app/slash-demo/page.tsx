a88bd131cb880b1f9581804a4cd3c5d6c2b887b6
Ravikrishna <ravikrishnaj25@gmail.com>
2026-05-01 11:44:34 +0530

    Remove slash-demo and fix Task types

--- app/slash-demo/page.tsx
+++
@@ -1,9 +0,0 @@
-import MinimalSlash from "@/components/animations/MinimalSlash";
-
-export default function SlashDemoPage() {
-	return (
-		<main className="min-h-screen bg-black flex items-center justify-center">
-			<MinimalSlash />
-		</main>
-	);
-}
